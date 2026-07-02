da4deddd80139871f9680e16ddfc94a1ffe163a7
nk960290-ui <nk960290@gmail.com>
2026-07-02 11:35:41 +0530

    Add files via upload

---
+++ dis only name.js
@@ -0,0 +1,18 @@
+let employees = [
+{
+    name: "Naveen",
+    experience: 2
+},
+{
+    name: "John",
+    experience: 7
+},
+{
+    name: "Arun",
+    experience: 5
+}
+];
+
+employees.forEach(employee => {
+    console.log(employee.name);
+});
